33d9b130c54d0b0799a532ccbd142bab776ecd6e
Roi Avinoam <avinoamr@gmail.com>
2014-02-07 17:23:23 +0200

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ connect-backbone
 ================
 
 A middleware for creating RESTful APIs in Connect and Express applications that uses Backbone Models
+
+# Usage
+
+```javascript
+var connect = require("connect");
+var backbone = require("backbone");
+var connect_backbone = require("connect-backbone");
+
+var MyModel = backbone.Model.extend({ ... });
+var MyCollection = backbone.Collection.extend({
+    model: MyModel
+});
+
+connect()
+    .use( "/my", connect_backbone( MyCollection ) )
+    .listen( 8000 );
+```
---
+++ index.js
@@ -0,0 +1,66 @@
+module.exports = function( Collection ) {
+    var Model = new Collection().model;
+
+    var uri = function( uri ) {
+        return uri.replace( /^\/|\/$/g, "" );
+    };
+
+    var post = function( req, res ) {
+        if ( !uri( req.url ) ) {
+            res.writeHead( 400 );
+            return res.end();
+        }
+
+        new Collection()
+            .create( req.body )
+            .on( "sync", function() {
+                res.end( JSON.stringify( this ) );
+            });
+    };
+
+    var get = function( req, res ) {
+        if ( req.url == "/" ) {
+            new Collection()
+                .on( "sync", function() {
+                    res.end( JSON.stringify( this ) );
+                } ).fetch({ data: req.query });
+        } else {
+            var m = new Model({ id: uri( req.url });
+            new Collection().add( m );
+            m.on( "sync", function() {
+                res.end( JSON.stringify( this ) );
+            }).fetch();
+        }
+    };
+
+    var put = function( req, res ) {
+        if ( req.url == "/" ) {
+            return post( req, res ); // PUT can be used for creation as well
+        }
+        var m = new Model({ id: uri( req.url });
+        new Collection().add( m );
+        m.on( "sync", function() {
+            res.end( JSON.stringify( this ) );
+        }).save( req.body );
+    };
+
+    var remove = function( req, res ) {
+        if ( req.url == "/" ) {
+            return post( req, res ); // PUT can be used for creation as well
+        }
+        var m = new Model({ id: uri( req.url });
+        new Collection().add( m );
+        m.on( "sync", function() {
+            res.end( JSON.stringify( this ) );
+        }).destroy();
+    };
+
+    return function( req, res, next ) {
+        return {
+            "POST": post,
+            "PUT": put,
+            "GET": get,
+            "DELETE": remove
+        }[ req.method ]( req, res );
+    }
+};
