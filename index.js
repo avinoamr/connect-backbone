76014f8beb6f1abb0d3010a2d97884261f4d36ac
Roi Avinoam <avinoamr@gmail.com>
2014-03-02 14:43:10 +0200

    Completely simplify the algorithm to only extend the request object instead of behaving as a stand-alone REST app

--- index.js
+++ index.js
@@ -1,82 +1,20 @@
 var url = require( "url" );
-
-// IMPORTANT NOTE: This method will give the client unrestricted ability to
-// modify the data and query the database - take steps to apply the relevant
-// restrictions prior to using this middleware.
 module.exports = function ( Collection ) {
-    var map = null;
-    var is_rest = false;
-
-    // it's advised to use the `connect-callback` package as it supports a wide
-    // variety of RESTful error codes and responses
-    var callback = function( res, err, out ) {
-        if ( map && out ) {
-            if ( out.models ) out.models = out.models.map( map );
-            else if ( out.attributes ) out = map( out );
-        }
-        if ( res.callback ) { return res.callback( err, out ); }
-        if ( err ) {
-            res.writeHead( 500, "Internal Server Error" );
-        }
-        if ( out ) {
-            if ( typeof out != "string" ) {
-                out = JSON.stringify( out );
-            }
-            res.write( out );
-        }
-        res.end()
-    };
-
-    // middleware to extend the request object with a model or collection
-    var middleware = function( req, res, next ) {
+    return function( req, res, next ) {
 
         // extract the resource ID from the url
-        var id = url.parse( req.url ).pathname.replace( /^\/|\/$/g, "" )
-                    .split( "/" )[ 0 ]; // just take the first part
+        var path = req.path || req.url.split( /\?|\&/ )[ 0 ]; // strip query-string
+        path = path.replace( /^\/|\/$/g, "" ).split( "/" ); // strip wrapping slashes
+        var id = path[ 0 ]; // just take the first part
+        var collection = new Collection();
 
-        var collection = new Collection()
-            .on( "destroy", ( m ) -> callback( req ) )
-            .on( "error invalid", ( m, err ) -> callback( req, err ) )
-
-        // create
         if ( !id && req.method == "POST" ) {
-            req.model = collection.add( new collection.model() )
-            return next()
+            req.model = new collection.model()
+        } else if ( id ) {
+            req.model = new collection.model( { id: id } );
+        } else {
+            req.collection = collection
         }
-
-        var query = id ? { id: id } : req.query;
-        collection.fetch({ data: query, success: function( c ) {
-            if ( id ) {
-                req.model = c.models[ 0 ] || c.add( new c.model( { id: id } ) );
-            } else {
-                req.collection = c;
-            }
-            if ( !is_rest ) return next();
-
-            // restful response
-            if ( req.model && [ "POST", "PUT", "PATCH" ].indexOf( req.method ) != -1 ) {
-                req.model.save( req.body, {
-                    success: function( m ) { callback( res, null, m ); }
-                });
-            } else if ( req.method == "GET" ) {
-                callback( res, req.model || req.collection );
-            } else if ( req.model && req.method == "DELETE" ) {
-                req.model.destroy({
-                    success: function( m ) { callback( res, null, m ); }
-                })
-            } else {
-                callback( true, "Unknown request" );
-            }
-
-        });
-    };
-
-    // middleware to completely handle RESTful requests to modify these
-    // resources.
-    middleware.rest = function( fn ) {
-        if ( fn ) map = fn;
-        return middleware;
-    };
-
-    return middleware;
+        next()
+    }
 };
--- test.js
+++ test.js
@@ -26,225 +26,222 @@ describe( "connect-backbone", function() {
         }
     });
 
-    it( "skips unknown methods", function( done ) {
-        connect.backbone()({
-            url: "", method: "hello"
-        }, null, done )
-    });
+    // it( "skips unknown methods", function( done ) {
+    //     var req = { url: "", method: "hello" };
+    //     connect.backbone( Collection ).rest()( req, {}, function() {
+    //         assert( req.collection instanceof Collection )
+    //         done();
+    //     } )
+    // });
 
 
     it( "skips invalid uris", function( done ) {
-        connect.backbone()({
-            url: "/hello/world/", method: "GET"
-        }, null, done );
-    });
-
-
-    it( "extends the request with the URI", function( done ) {
-        var req = { url: "/okay/hello/", method: "GET" };
-        connect.backbone()( req, null, function() {
-            assert.equal( req.uri, "okay/hello" );
+        var req = { url: "/hello/world/", method: "GET" };
+        var res = { write: function() { assert.fail() } };
+        connect.backbone( Collection ).rest()( req, res, function() {
+            assert( req.model instanceof Collection.prototype.model );
+            assert.equal( req.model.id, "hello" );
             done();
-        } );
-    });
-
-
-    it( "strips the query string", function( done ) {
-        var req = { url: "/okay/hello/?foo=bar", method: "GET" };
-        connect.backbone()( req, null, function() {
-            assert.equal( req.uri, "okay/hello" );
-            done();
-        } );
-    });
-
-
-    it( "creates a Model with POST", function( done ) {
-        var out = "";
-        connect.backbone( Model, Collection )(
-            { url: "/", method: "POST", body: { hello: "world" } },
-            {
-                write: function( s ) { out += s.toString() },
-                end: function() {
-                    out = JSON.parse( out )
-                    assert.equal( out.hello, "world" );
-                    assert( out.id );
-                    var doc = db[ Model.prototype.urlRoot ][ out.id ];
-                    assert.deepEqual( doc, out );
-                    done();
-                }
-            }
-        );
-    });
-
-
-    it( "creates a Model with PUT", function( done ) {
-        var out = "";
-        connect.backbone( Model, Collection )(
-            { url: "/cookie", method: "PUT", body: { hello: "world" } },
-            {
-                write: function( s ) { out += s.toString() },
-                end: function() {
-                    out = JSON.parse( out )
-                    assert.equal( out.hello, "world" );
-                    assert.equal( out.id, "cookie" );
-                    var doc = db[ Model.prototype.urlRoot ][ out.id ];
-                    assert.deepEqual( doc, out );
-                    done();
-                }
-            }
-        );
-    });
-
-    it( "fails to create an invalid Model", function( done ) {
-        var InvalidModel = Model.extend({
-            validate: function() {
-                return "Invalid";
-            }
-        });
-        connect.backbone( InvalidModel, Collection )(
-            { url: "/cookie", method: "PUT", body: { hello: "world" } },
-            {
-                writeHead: function( code ) {
-                    assert.equal( code, 400 ); // Bad Request
-                    done();
-                },
-                write: function() {},
-                end: function() {}
-            }
-        );
-    })
-
-
-    it( "updates a Model with PUT", function( done ) {
-        new Model({ id: "cookie" }).save({ foo: "bar" }, {
-            success: function() {
-                var out = "";
-                connect.backbone( Model, Collection )(
-                    { url: "/cookie", method: "PUT", body: { hello: "world" } },
-                    {
-                        write: function( s ) { out += s.toString() },
-                        end: function() {
-                            out = JSON.parse( out )
-                            assert.equal( out.hello, "world" );
-                            assert.equal( out.id, "cookie" );
-                            var doc = db[ Model.prototype.urlRoot ][ out.id ];
-                            assert.deepEqual( doc, out );
-                            assert( !out.foo ); // overridden
-                            done();
-                        }
-                    }
-                );
-            }
         });
     });
 
 
-    it( "extends a Model with PATCH", function( done ) {
-        new Model({ id: "cookie" }).save({ foo: "bar" }, {
-            success: function() {
-                var out = "";
-                connect.backbone( Model, Collection )(
-                    { url: "/cookie", method: "PATCH", body: { hello: "world" } },
-                    {
-                        write: function( s ) { out += s.toString() },
-                        end: function() {
-                            out = JSON.parse( out )
-                            assert.equal( out.hello, "world" );
-                            assert.equal( out.foo, "bar" );
-                            assert.equal( out.id, "cookie" );
-                            var doc = db[ Model.prototype.urlRoot ][ out.id ];
-                            assert.deepEqual( doc, out );
-                            done();
-                        }
-                    }
-                );
-            }
-        });
-    });
+    // it( "strips the query string", function( done ) {
+    //     var req = { url: "/okay/hello/?foo=bar", method: "GET" };
+    //     connect.backbone( Collection )( req, {}, function() {
+    //         assert.equal( req.model.id, "okay" );
+    //         done();
+    //     } );
+    // });
 
 
-    it( "deletes a Model with DELETE", function( done ) {
-        new Model({ id: "cookie" }).save({ foo: "bar" }, {
-            success: function() {
-                var out = "";
-                connect.backbone( Model, Collection )(
-                    { url: "/cookie", method: "DELETE" },
-                    {
-                        write: function( s ) { out += s.toString() },
-                        end: function() {
-                            out = JSON.parse( out )
-                            assert.equal( out.id, "cookie" );
-                            var doc = db[ Model.prototype.urlRoot ][ out.id ];
-                            assert( !doc );
-                            done();
-                        }
-                    }
-                );
-            }
-        });
-    });
+    // it( "creates a Model with POST", function( done ) {
+    //     var out = "";
+    //     connect.backbone( Collection ).rest()(
+    //         { url: "/", method: "POST", body: { hello: "world" } },
+    //         {
+    //             write: function( s ) { out += s.toString() },
+    //             end: function() {
+    //                 out = JSON.parse( out )
+    //                 assert.equal( out.hello, "world" );
+    //                 assert( out.id );
+    //                 var doc = db[ Model.prototype.urlRoot ][ out.id ];
+    //                 assert.deepEqual( doc, out );
+    //                 done();
+    //             }
+    //         }
+    //     );
+    // });
 
 
-    it( "reads a Model with GET", function( done ) {
-        new Model({ id: "cookie" }).save({ foo: "bar" }, {
-            success: function() {
-                var out = "";
-                connect.backbone( Model, Collection )(
-                    { url: "/cookie", method: "GET" },
-                    {
-                        write: function( s ) { out += s.toString() },
-                        end: function() {
-                            out = JSON.parse( out )
-                            assert.equal( out.id, "cookie" );
-                            assert.equal( out.foo, "bar" );
-                            var doc = db[ Model.prototype.urlRoot ][ out.id ];
-                            assert.deepEqual( doc, out );
-                            done();
-                        }
-                    }
-                );
-            }
-        });
-    });
+    // it( "creates a Model with PUT", function( done ) {
+    //     var out = "";
+    //     connect.backbone( Collection )(
+    //         { url: "/cookie", method: "PUT", body: { hello: "world" } },
+    //         {
+    //             write: function( s ) { out += s.toString() },
+    //             end: function() {
+    //                 out = JSON.parse( out )
+    //                 assert.equal( out.hello, "world" );
+    //                 assert.equal( out.id, "cookie" );
+    //                 var doc = db[ Model.prototype.urlRoot ][ out.id ];
+    //                 assert.deepEqual( doc, out );
+    //                 done();
+    //             }
+    //         }
+    //     );
+    // });
 
-    it( "404 when a Model is not found with GET", function( done ) {
-        var out = "";
-        connect.backbone( Model, Collection )(
-            { url: "/cookie", method: "GET" },
-            {
-                writeHead: function( code, reason ) {
-                    assert.equal( code, 404 );
-                    done()
-                },
-                write: function() {},
-                end: function() {}
-            }
-        );
-    });
+    // it( "fails to create an invalid Model", function( done ) {
+    //     var InvalidModel = Model.extend({
+    //         validate: function() {
+    //             return "Invalid";
+    //         }
+    //     });
+    //     connect.backbone( Collection )(
+    //         { url: "/cookie", method: "PUT", body: { hello: "world" } },
+    //         {
+    //             writeHead: function( code ) {
+    //                 assert.equal( code, 400 ); // Bad Request
+    //                 done();
+    //             },
+    //             write: function() {},
+    //             end: function() {}
+    //         }
+    //     );
+    // })
 
 
-    it( "searches a Collection with GET", function( done ) {
-        new Model({ id: "cookie" }).save({ foo: "bar" }, {
-            success: function() {
-                var out = "";
-                connect.backbone( Model, Collection )(
-                    { url: "/", method: "GET" },
-                    {
-                        write: function( s ) { out += s.toString() },
-                        end: function() {
-                            out = JSON.parse( out )
-                            assert.equal( out.length, 1 );
-                            out = out[ 0 ];
-                            assert.equal( out.foo, "bar" );
-                            var doc = db[ Model.prototype.urlRoot ][ out.id ];
-                            assert.deepEqual( doc, out );
-                            done();
-                        }
-                    }
-                );
-            }
-        });
-    });
+    // it( "updates a Model with PUT", function( done ) {
+    //     new Model({ id: "cookie" }).save({ foo: "bar" }, {
+    //         success: function() {
+    //             var out = "";
+    //             connect.backbone( Collection )(
+    //                 { url: "/cookie", method: "PUT", body: { hello: "world" } },
+    //                 {
+    //                     write: function( s ) { out += s.toString() },
+    //                     end: function() {
+    //                         out = JSON.parse( out )
+    //                         assert.equal( out.hello, "world" );
+    //                         assert.equal( out.id, "cookie" );
+    //                         var doc = db[ Model.prototype.urlRoot ][ out.id ];
+    //                         assert.deepEqual( doc, out );
+    //                         assert( !out.foo ); // overridden
+    //                         done();
+    //                     }
+    //                 }
+    //             );
+    //         }
+    //     });
+    // });
+
+
+    // it( "extends a Model with PATCH", function( done ) {
+    //     new Model({ id: "cookie" }).save({ foo: "bar" }, {
+    //         success: function() {
+    //             var out = "";
+    //             connect.backbone( Collection )(
+    //                 { url: "/cookie", method: "PATCH", body: { hello: "world" } },
+    //                 {
+    //                     write: function( s ) { out += s.toString() },
+    //                     end: function() {
+    //                         out = JSON.parse( out )
+    //                         assert.equal( out.hello, "world" );
+    //                         assert.equal( out.foo, "bar" );
+    //                         assert.equal( out.id, "cookie" );
+    //                         var doc = db[ Model.prototype.urlRoot ][ out.id ];
+    //                         assert.deepEqual( doc, out );
+    //                         done();
+    //                     }
+    //                 }
+    //             );
+    //         }
+    //     });
+    // });
+
+
+    // it( "deletes a Model with DELETE", function( done ) {
+    //     new Model({ id: "cookie" }).save({ foo: "bar" }, {
+    //         success: function() {
+    //             var out = "";
+    //             connect.backbone( Collection )(
+    //                 { url: "/cookie", method: "DELETE" },
+    //                 {
+    //                     write: function( s ) { out += s.toString() },
+    //                     end: function() {
+    //                         out = JSON.parse( out )
+    //                         assert.equal( out.id, "cookie" );
+    //                         var doc = db[ Model.prototype.urlRoot ][ out.id ];
+    //                         assert( !doc );
+    //                         done();
+    //                     }
+    //                 }
+    //             );
+    //         }
+    //     });
+    // });
+
+
+    // it( "reads a Model with GET", function( done ) {
+    //     new Model({ id: "cookie" }).save({ foo: "bar" }, {
+    //         success: function() {
+    //             var out = "";
+    //             connect.backbone( Collection )(
+    //                 { url: "/cookie", method: "GET" },
+    //                 {
+    //                     write: function( s ) { out += s.toString() },
+    //                     end: function() {
+    //                         out = JSON.parse( out )
+    //                         assert.equal( out.id, "cookie" );
+    //                         assert.equal( out.foo, "bar" );
+    //                         var doc = db[ Model.prototype.urlRoot ][ out.id ];
+    //                         assert.deepEqual( doc, out );
+    //                         done();
+    //                     }
+    //                 }
+    //             );
+    //         }
+    //     });
+    // });
+
+    // it( "404 when a Model is not found with GET", function( done ) {
+    //     var out = "";
+    //     connect.backbone( Collection )(
+    //         { url: "/cookie", method: "GET" },
+    //         {
+    //             writeHead: function( code, reason ) {
+    //                 assert.equal( code, 404 );
+    //                 done()
+    //             },
+    //             write: function() {},
+    //             end: function() {}
+    //         }
+    //     );
+    // });
+
+
+    // it( "searches a Collection with GET", function( done ) {
+    //     new Model({ id: "cookie" }).save({ foo: "bar" }, {
+    //         success: function() {
+    //             var out = "";
+    //             connect.backbone( Collection )(
+    //                 { url: "/", method: "GET" },
+    //                 {
+    //                     write: function( s ) { out += s.toString() },
+    //                     end: function() {
+    //                         out = JSON.parse( out )
+    //                         assert.equal( out.length, 1 );
+    //                         out = out[ 0 ];
+    //                         assert.equal( out.foo, "bar" );
+    //                         var doc = db[ Model.prototype.urlRoot ][ out.id ];
+    //                         assert.deepEqual( doc, out );
+    //                         done();
+    //                     }
+    //                 }
+    //             );
+    //         }
+    //     });
+    // });
 
 
 });
