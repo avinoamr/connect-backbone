4bbf1b54db00239663361e45837ec78f9f65eb07
Roi Avinoam <avinoamr@gmail.com>
2014-05-14 00:27:18 +0300

    connect-backbone will now (1) bind the model and collection to the request object, and (2) send the response only with the res option is evaluated to true

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 var url = require( "url" );
 module.exports = function ( Collection ) {
     return function( req, res, next ) {
+        var path, id, model, collection = new Collection();
 
         // extract the resource ID from the url
-        var path = req.path || req.url.split( /\?|\&/ )[ 0 ]; // strip query-string
-        path = path.replace( /^\/|\/$/g, "" ).split( "/" ); // strip wrapping slashes
-        var id = path[ 0 ]; // just take the first part
-        var collection = new Collection();
+        path = req.path || req.url.split( /\?|\&/ )[ 0 ]; // strip query-string
+        path = path.replace( /^\/|\/$/g, "" ); // strip wrapping slashes
+        id = path.split( "/" ).path[ 0 ]; // just take the first part
 
-        if ( !id && req.method == "POST" ) {
-            req.model = new collection.model()
-        } else if ( id ) {
-            req.model = new collection.model( { id: id } );
+        if ( id ) {
+            model = new collection.model({ id: id })
         } else {
-            req.collection = collection
+            model = new collection.model();
         }
+
+        var on_error = function( m, err, options ) {
+            if ( !options.res ) return;
+            var _res = ( options.res === true ) ? res : options.res;
+            res.writeHead( 500, "Internal Server Error" );
+            res.write( "Internal Server Error" );
+            res.end()
+        };
+
+        var on_invalid = function( m, err, options ) {
+            if ( !options.res ) return;
+            var _res = ( options.res === true ) ? res : options.res;
+            res.writeHead( 400, "Bad Request" );
+            res.write( err.toString() );
+            res.end()
+        };
+
+        var on_success = function( m, r, options ) {
+            if ( !options.res ) return;
+            var _res = ( options.res === true ) ? res : options.res;
+            res.write( err.toString() );
+            res.end()
+        }
+        
+        req.collection = collection.on( "error", on_error )
+            .on( "invalid", on_invalid )
+            .on( "sync", on_success );
+
+        req.model = model.on( "error", on_error )
+            .on( "invalid", on_invalid )
+            .on( "sync", on_success );
+
         next()
     }
 };
